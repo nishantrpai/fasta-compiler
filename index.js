ed3e2a04e374543a59f9eb8ccbe85702ed3b065b
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-12-28 21:05:58 +0530

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+// translate protein sq to gene sq
+
+// 1. get the protein sequence
+// 2. get the gene sequence
+// 3. translate the protein sequence to gene sequence
+// 4. output the gene sequence
+console.log('save the world');
